70d7c9a39e97d7e54d0ecb31193e47007c43990d
Dmitry <k1791@mail.ru>
2023-04-26 22:31:55 +0500

    step1

---
+++ data/API_connect.py
@@ -0,0 +1,134 @@
+import os
+import json
+from abc import ABC, abstractmethod
+import time
+from requests import get, post, put, delete
+import psycopg2
+
+class Engine(ABC):
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+    @abstractmethod
+    def get_page(self, page=0):
+        pass
+class Vacancy ():
+    """"Класс Vacancy"""
+
+
+    def __init__(self, search_query,top):
+
+        self.search_query =search_query #  поисковый запрос
+        self.top = top # количество вакансий для вывода
+        self.vacancy = self.get_vacancies()
+
+        pass
+
+    def get_vacancies(self):
+
+
+        if self.platforms in [1, 3]: # загрузка платформы 1 - "HeadHunter"
+            hh_vacancy = HeadHunterAPI(self.search_query,self.top)
+
+            for i in hh_vacancy:
+                for ii in i.get('items'):
+                    try:
+                        self.list.append({
+                                "payment_from": int(ii.get("salary", {}).get("from")),  # Сумма оклада от
+                                "payment_to": int(ii.get("salary", {}).get("to")),  # Сумма оклада до
+                                "currency": ii.get("salary", {}).get("currency"), # Валюта. Список возможных значений:  rub — рубль  uah — гривна  uzs — сум
+                                "platforms":"HeadHunter", # платформа ["HeadHunter", "SuperJob"]
+                                 "profession":ii.get("name"), # название вакансии
+                                 "candidat":ii.get("snippet",{}).get("requirement"), # 	Требования к кандидату
+                                 "work": ii.get("snippet",{}).get("responsibility"), # Должностные обязанности
+                                 "compensation": ii.get("working_days"), # Условия работы
+                                 "profession_url":ii.get("url") # ссылка на вакансию
+                                                                   })
+                    except:
+                       raise
+
+
+    def __repr__(self):
+        return f'Vacancy({self.search_query}, {self.top_n})'
+
+    def __str__(self):
+        return f'{self.search_query}, {self.top_n}'
+
+
+
+
+class HeadHunterAPI (Engine):
+    """"Класс HeadHunterAPI"""
+    list = []
+
+    def __init__(self, vacancy, top_n):
+        self.vacancy = vacancy
+        self.url ='https://api.hh.ru/vacancies'
+        self.pages = int(-1 * top_n // 1 * -1)
+        self.job_list = self.get_vacancies()
+
+        pass
+
+    def get_page(self, page = 0):
+        """
+          метод для получения страницы со списком вакансий.
+          Аргументы: page - Индекс страницы, начинается с 0. Значение по умолчанию 0, т.е. первая страница
+          """
+        # Справочник для параметров GET-запроса
+        par = {
+            'text': self.vacancy, # Текст фильтра. В имени должно быть слово job_title
+            'area': '1', # Поиск ощуществляется по вакансиям htubjyf 113 (1 - город Москва)
+            'per_page': '20', # Кол-во вакансий на 1 странице
+            'page': page # Индекс страницы поиска на HH
+               }
+
+        req = get(self.url, params=par)  # Посылаем запрос к API
+        data = req.json()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
+        # проверка на наличие данных на странице
+        try:
+            if  data.get('items',{})[0].get('id') is not None:
+                self.list.append(data)
+        except:
+            raise
+        req.close()
+        return data
+
+
+    def get_vacancies(self):
+        """переноса агруженных данных в PostgreSQL"""
+
+        for page in range(0, self.pages):
+
+            # Преобразуем текст ответа запроса в справочник Python
+            r_page = self.get_page(page)
+
+            # Проверка на наличие данных на странице
+            try:
+                if r_page.get('items',{})[0].get('id') is None:
+                    break
+            except:
+                raise
+
+            # Необязательная задержка, но чтобы не нагружать сервисы hh, оставим. 5 сек мы может подождать
+            time.sleep(0.5)
+
+        # Создаем первичную базу данных
+        conn = psycopg2.connect(host='localhost',  user='postgres', password='171717')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    # execute query
+                    cur.execute("CREATE DATABASE BASE")
+                    for row in self.list:
+                        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                    (row['payment_from'], row['payment_to'], row['name']))
+        except:
+            raise
+        finally:
+            conn.close()
+
+
+
+        print('Вакансии HeadHunter сохранены в файл')
+        return json.dumps(self.list, ensure_ascii=False)
--- function/utils.py
+++ function/utils.py
@@ -1,116 +1,49 @@
+from requests import get, post, put, delete
+import json
+import psycopg2
 
-# from data.class_chanell import Channel
-# from data.class_chanell import PLVideo
-from data.class_chanell import Video
-from data.class_chanell import PlayList
-from datetime import time, timedelta
+list = []
+def get_vacancies():
+    url ='https://api.hh.ru/vacancies' # запрос вакансий HH
+    vacancy = input (f'введите искомую вакансию \n')
+    page =0
+    # Справочник для параметров GET-запроса
+    par = {
+        'text': vacancy, # Текст фильтра. В имени должно быть слово job_title
+        'area': '1', # Поиск ощуществляется по вакансиям htubjyf 113 (1 - город Москва)
+        'per_page': '20', # Кол-во вакансий на 1 странице
+        'page': page # Индекс страницы поиска на HH
+           }
 
+    req = get(url, params=par)  # Посылаем запрос к API
+    data = req.json()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
+    # проверка на наличие данных на странице
+    try:
+        if  data.get('items',{})[0].get('id') is not None:
+            list.append(data)
+    except:
+        pass
+    req.close()
 
-broken_video = Video('broken_video_id')
-print(broken_video.video_id)
-# 'broken_video_id'
-print(broken_video.title)
-# None
-print(broken_video.like_count)
-# None
-
-
-
-#
-# pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
-#
-# print(pl.plv)
-# # Редакция. АнтиТревел
-#
-# print(pl.url)
-# # https://www.youtube.com/playlist?list=PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb
-#
-# # print(pl.print_info())
-# duration = pl.total_duration
-# print(duration)
-# # 3:41:01
-# print(type(duration))
-# # # <class 'datetime.timedelta'>
-# print(duration.total_seconds())
-# # # 13261.0
-# #
-# pl.show_best_video()
-# # # https://youtu.be/9Bv2zltQKQA
+    return list
 
 
+# Создаем подключение к PosgrySQL
+conn = psycopg2.connect(host='localhost', user='postgres', password='171717')
 
+# включаем автоматическое сохранение изменений в БД
+conn.autocommit = True
+data_base_name = 'base'
 
+try:
+    with conn.cursor() as cur:
+        # проверка на наличе БД с требуемым именем
+        exists = cur.execute(f"SELECT COUNT(*) = 0 FROM pg_catalog.pg_database WHERE datname = '{data_base_name}'")
+        # если такой БД нет мы ее создаем
+        if not exists:
+            cur.execute(f'CREATE DATABASE  "{data_base_name}"')
 
-
-
-
-
-
-
-#
-#
-#
-#
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
-# vdud = Channel(channel_id)
-#
-# # получаем значения атрибутов
-# print(vdud.title)
-# # вДудь
-# print(vdud.video_count)
-# # 163
-# print(vdud.url)
-# # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
-#
-# # менять не можем
-# vdud.channel_id = 'Новое название'
-# # AttributeError: property 'channel_id' of 'Channel' object has no setter
-#
-# # можем получить объект для работы с API вне класса
-# print(Channel.get_service())
-# # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
-#
-# # print(vdud.print_info())
-#
-# # создать файл 'vdud.json' в данными по каналу
-# vdud.to_json('vdud.json')
-#
-# ch1=Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-# ch2=Channel('UCXD8fPuwMfizCmgMtc9KAqQ')
-#
-#
-#
-# print(ch1)
-# #Youtube-канал: вДудь
-# # print(repr(ch1))
-#
-# print(ch2)
-# #Youtube-канал: Редакция
-# # print(repr(ch2))
-#
-#
-# ch1 > ch2
-# #True
-#
-# ch1 < ch2
-# #False
-#
-# ch1 + ch2
-# #1030000076
-#
-# # https://www.youtube.com/watch?v=9lO06Zxhu88
-# video1 = Video('9lO06Zxhu88')  # '9lO06Zxhu88' - это id видео из ютуб "etag": "GQNNYQsMXoegKlWQYVAogTvQdXM"
-#
-# # print(video1.print_info())
-# print(video1)
-# # print(repr(video1))
-#
-# video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
-# #https://www.youtube.com/watch?v=BBotskuyw_M
-# #print(video1)
-# #Как устроена IT-столица мира / Russian Silicon Valley (English subs)
-# # print(video2.print_info())
-# print(video2)
-# # print(repr(video2))
-# #Пушкин: наше все? (Литература)
-# # шаблон: 'название_видео (название_плейлиста)
+except:
+    raise
+finally:
+    conn.close()
