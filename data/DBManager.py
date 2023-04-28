99e74b46758ef73c5776d9be059b6ebfd11a0caa
Dmitry <k1791@mail.ru>
2023-04-28 14:11:35 +0500

    step13 get_companies_and_vacancies_count()

--- data/DBManager.py
+++ data/DBManager.py
@@ -1,5 +1,5 @@
 import psycopg2
-import data.queries
+# import data.queries
 
 
 class DBManager:
@@ -7,12 +7,27 @@ class DBManager:
 
     def __init__(self):
         self.base = 'base'  # Название используемой БД
+        self.conn = psycopg2.connect(dbname=f'{self.base}', host='localhost', user='postgres', password='171717')
         pass
 
     def get_companies_and_vacancies_count(self):
         """получает список всех компаний и количество вакансий у каждой компании"""
-        pass
 
+        try:
+            with self.conn.cursor() as cur:
+                # формируем запрос
+                cur.execute(f"select DISTINCT Employer.employer_name, count(Vacancy.name) as vacancy "
+                            "from employer Join vacancy USING (id_employer)"
+                            "GROUP BY Employer.employer_name")
+                reqest = cur.fetchall()
+
+
+        except:
+            raise
+        finally:
+            self.conn.close()
+
+        return reqest
     def get_all_vacancies(self):
         """получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию"""
         pass
--- function/utils.py
+++ function/utils.py
@@ -2,9 +2,19 @@ from requests import get, post, put, delete
 import json
 import psycopg2
 from data.API_connect import Vacancy
+from data.DBManager import DBManager
+
+# инициализируем начальные данные запроса к API
+# vacancy = input (f'введите искомую вакансию \n')
+# top = int(input (f'введите объем списка вакансий \n'))
+# vac = Vacancy(vacancy, top)
+# vac.get_vacancies()
+
+# Запускаем класс DBManager и оформляем запросы к созданной БД
+req = DBManager ()
+
+"""получает список всех компаний и количество вакансий у каждой компании"""
+for row in req.get_companies_and_vacancies_count():
+    print (row)
 
 
-vacancy = input (f'введите искомую вакансию \n')
-top = int(input (f'введите объем списка вакансий \n'))
-vac = Vacancy(vacancy, top)
-vac.get_vacancies()
