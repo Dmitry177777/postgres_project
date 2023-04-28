983500c5a99c28908e354ab3ff18f933fef3fd54
Dmitry <k1791@mail.ru>
2023-04-28 14:49:51 +0500

    step15 get_vacancies_with_higher_salary

--- data/DBManager.py
+++ data/DBManager.py
@@ -40,7 +40,6 @@ class DBManager:
                             "from employer Join vacancy USING (id_employer)")
                 reqest = cur.fetchall()
 
-
         except:
             raise
         finally:
@@ -50,12 +49,54 @@ class DBManager:
 
     def get_avg_salary(self):
         """получает среднюю зарплату по вакансиям"""
-        pass
+        conn = self.bd()
+        try:
+            with conn.cursor() as cur:
+                # формируем запрос
+                cur.execute(f"select  Vacancy.name, avg(Vacancy.salary_from) as salary_from, avg(Vacancy.salary_to) as salary_to "
+                            "from vacancy "
+                            "GROUP BY Vacancy.name")
+                reqest = cur.fetchall()
+
+        except:
+            raise
+        finally:
+            conn.close()
+
+        return reqest
 
     def get_vacancies_with_higher_salary(self):
         """получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        pass
+        conn = self.bd()
+        try:
+            with conn.cursor() as cur:
+                # формируем запрос
+                cur.execute(f"select  Vacancy.name, Employer.employer_name, Vacancy.salary_from, Vacancy.salary_to "
+                            "from vacancy Join employer USING (id_employer) "
+                            "Where Vacancy.salary_from > (select avg(salary_from) from vacancy )")
+                reqest = cur.fetchall()
 
-    def get_vacancies_with_keyword(self):
+        except:
+            raise
+        finally:
+            conn.close()
+
+        return reqest
+
+    def get_vacancies_with_keyword(self, ):
         """получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”"""
-        pass
+        conn = self.bd()
+        try:
+            with conn.cursor() as cur:
+                # формируем запрос
+                cur.execute(f"select  Vacancy.name, Employer.employer_name, Vacancy.salary_from, Vacancy.salary_to "
+                            "from vacancy Join employer USING (id_employer) "
+                            "Where Vacancy.salary_from > (select avg(salary_from) from vacancy )")
+                reqest = cur.fetchall()
+
+        except:
+            raise
+        finally:
+            conn.close()
+
+        return reqest
